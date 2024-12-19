eace269b889d2cee8842b5fadd767b3859338cef
VThinh123 <programthinh@gmail.com>
2024-12-19 13:03:42 +0700

    Code LL

---
+++ linkedlist/LinkedList.java
@@ -0,0 +1,81 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package linkedlist;
+
+
+public class LinkedList {
+    Node head;
+
+    public void addFirst(int data){
+        Node newNode = new Node(data);
+        if(head == null){
+            head = newNode;
+        }else{
+            newNode.next = head;
+            head = newNode;
+        }
+    }
+    
+    public void addLast(int data){
+        Node NewNode = new Node(data);
+        if(head == null){
+            head = NewNode;
+            return;
+        }
+        Node temp = head;
+        while(temp.next != null){
+            temp = temp.next;
+        }
+        temp.next = NewNode;
+    }
+    
+    public void printListNode(){
+        Node list = head;
+        while (list != null){
+            System.out.print(list.getData() + "-->");
+            list = list.next;
+        }
+    }
+    
+    public int getNodeByPosition(int index){
+        if(index < 0){
+            System.out.print("Index must postive number");
+            return 0;
+        }
+        Node temp = head;
+        int count = 0;
+        while (temp != null){
+            if(count == index){
+                return temp.getData();
+            }
+            count++;
+            temp = temp.next;
+        }
+        return 0;
+    }
+    public static void main(String[] args) {
+        LinkedList ListNode = new LinkedList();
+        //in ra node tu lon toi nho
+//        ListNode.addFirst(1);
+//        ListNode.addFirst(2);
+//        ListNode.addFirst(3);
+//        ListNode.addFirst(4);
+//        ListNode.addFirst(5);
+        
+        // in ra tu nho toi lon
+        ListNode.addLast(1);
+        ListNode.addLast(2);
+        ListNode.addLast(3);
+        ListNode.addLast(4);
+        ListNode.addLast(5);
+        
+        System.out.println("List node:");
+        ListNode.printListNode();
+        
+        int result = ListNode.getNodeByPosition(3);
+        System.out.println("\nValue at position 3:" + result);
+    }
+    
+}
---
+++ linkedlist/Node.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package linkedlist;
+
+/**
+ *
+ * @author LENOVO LEGION
+ */
+public class Node {
+    public int data;
+    public Node next;
+
+    public Node(Node next, int data) {
+        this.next = next;
+        this.data = data;
+    }
+
+    public Node(int data) {
+        this.data = data;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
+
+    public int getData() {
+        return data;
+    }
+
+    public void setData(int data) {
+        this.data = data;
+    }
+}
+
